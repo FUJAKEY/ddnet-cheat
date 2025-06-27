d42d80d1087000195bc647a353e5ad6e7ae5df7b
FUJAKEY <164060147+FUJAKEY@users.noreply.github.com>
2025-06-27 16:13:56 +0300

    Fix CFujixTas header

--- src/game/client/components/fujix_tas.h
+++ src/game/client/components/fujix_tas.h
@@ -13,53 +13,76 @@
 class CFujixTas : public CComponent
 {
 public:
-    static const char *ms_pFujixDir;
+static const char *ms_pFujixDir;
 
 private:
-private:
-    struct SEntry
-    {
-        int m_Tick;
-        CNetObj_PlayerInput m_Input;
-    };
+struct SEntry
+{
+int m_Tick;
+CNetObj_PlayerInput m_Input;
+};
 
-    bool m_Recording;
-    bool m_Playing;
-    bool m_Testing;
-    int m_StartTick;
-    int m_TestStartTick;
-    int m_PlayStartTick;
-    char m_aFilename[IO_MAX_PATH_LENGTH];
-    IOHANDLE m_File;
-    std::vector<SEntry> m_vEntries;
-    int m_PlayIndex;
-    int m_LastRecordTick;
-    CNetObj_PlayerInput m_LastInput;
-    CNetObj_PlayerInput m_CurrentInput;
-    bool m_StopPending;
-    int m_StopTick;
+bool m_Recording;
+bool m_Playing;
+bool m_Testing;
+int m_StartTick;
+int m_TestStartTick;
+int m_PlayStartTick;
+char m_aFilename[IO_MAX_PATH_LENGTH];
+IOHANDLE m_File;
+std::vector<SEntry> m_vEntries;
+int m_PlayIndex;
+int m_LastRecordTick;
+CNetObj_PlayerInput m_LastInput;
+CNetObj_PlayerInput m_CurrentInput;
+bool m_StopPending;
+int m_StopTick;
 
-    // Phantom
-    bool m_PhantomActive;
-    int m_PhantomTick;
-    CCharacterCore m_PhantomCore;
-    CCharacterCore m_PhantomPrevCore;
-    CTeeRenderInfo m_PhantomRenderInfo;
-    int m_PhantomStep;
-    CNetObj_PlayerInput m_PhantomInput;
-    int m_PhantomPlayIndex;
+// Phantom
+bool m_PhantomActive;
+int m_PhantomTick;
+CCharacterCore m_PhantomCore;
+CCharacterCore m_PhantomPrevCore;
+CTeeRenderInfo m_PhantomRenderInfo;
+int m_PhantomStep;
+CNetObj_PlayerInput m_PhantomInput;
+int m_PhantomPlayIndex;
 
+void GetPath(char *pBuf, int Size) const;
+void UpdatePlaybackInput();
+void TickPhantom();
+void CoreToCharacter(const CCharacterCore &Core, CNetObj_Character *pChar, int Tick);
+void FinishRecord();
+void RenderFuturePath(int TicksAhead);
+void TickPhantomUpTo(int TargetTick);
 
-    void GetPath(char *pBuf, int Size) const;
-    void UpdatePlaybackInput();
-    void TickPhantom();
-    void CoreToCharacter(const CCharacterCore &Core, CNetObj_Character *pChar, int Tick);
-    void FinishRecord();
-    void RenderFuturePath(int TicksAhead);
-    void TickPhantomUpTo(int TargetTick);
+static void ConRecord(IConsole::IResult *pResult, void *pUserData);
+static void ConPlay(IConsole::IResult *pResult, void *pUserData);
+static void ConTest(IConsole::IResult *pResult, void *pUserData);
 
+public:
+CFujixTas();
+virtual int Sizeof() const override { return sizeof(*this); }
 
-    static void ConRecord(IConsole::IResult *pResult, void *pUserData);
-    static void ConPlay(IConsole::IResult *pResult, void *pUserData);
-    static void ConTest(IConsole::IResult *pResult, void *pUserData);
+virtual void OnConsoleInit() override;
+virtual void OnMapLoad() override;
+virtual void OnUpdate() override;
+virtual void OnRender() override;
+
+void StartRecord();
+void StopRecord();
+void StartPlay();
+void StopPlay();
+void StartTest();
+void StopTest();
+bool IsRecording() const { return m_Recording; }
+bool IsPlaying() const { return m_Playing; }
+bool IsTesting() const { return m_Testing; }
+bool IsPhantomActive() const { return m_PhantomActive; }
+vec2 PhantomPos() const { return m_PhantomCore.m_Pos; }
+bool FetchPlaybackInput(CNetObj_PlayerInput *pInput);
+void RecordInput(const CNetObj_PlayerInput *pInput, int Tick);
+void MaybeFinishRecord();
+};
+
 #endif // GAME_CLIENT_COMPONENTS_FUJIX_TAS_H
