fddc636701955150cc04608dcf8c99ac23cd1391
FUJAKEY <164060147+FUJAKEY@users.noreply.github.com>
2025-07-21 18:00:04 +0300

    Remove freeze rage feature

--- src/game/client/components/menus_settings.cpp
+++ src/game/client/components/menus_settings.cpp
@@ -3556,12 +3556,6 @@ void CMenus::RenderSettingsFujix(CUIRect MainView)
            if(DoButton_CheckBox(&s_BlockChk, Localize("Block freeze (legit)"), g_Config.m_ClFujixBlockFreezeLegit, &BlockBox))
                    g_Config.m_ClFujixBlockFreezeLegit ^= 1;
 
-           MainView.HSplitTop(5.0f, nullptr, &MainView);
-           CUIRect RageBox;
-           MainView.HSplitTop(ms_ButtonHeight, &RageBox, &MainView);
-           static int s_RageChk = 0;
-           if(DoButton_CheckBox(&s_RageChk, Localize("Block freeze (rage)"), g_Config.m_ClFujixBlockFreezeRage, &RageBox))
-                   g_Config.m_ClFujixBlockFreezeRage ^= 1;
     }
        else if(m_FujixPage == 2)
        {
